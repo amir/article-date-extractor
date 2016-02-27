f1bf72e575578bda01346ffab81ec7c07fe04de4
Amir Mohammad Saied <amirsaied@gmail.com>
2016-02-27 17:35:01 +0000

    hlint

--- src/Web/ArticleDateExtractor.hs
+++ src/Web/ArticleDateExtractor.hs
@@ -31,35 +31,35 @@ readUrl url = do
   return rsp
 
 hasAttrValue' :: ArrowXml a => String -> String -> a XmlTree XmlTree
-hasAttrValue' a v = (hasAttrValue a (map toLower >>> (== v)))
+hasAttrValue' a v = hasAttrValue a (map toLower >>> (== v))
 
 extractFromLdJson doc = runX $ doc                  >>>
       css "script"                                  >>>
-      (hasAttrValue' "type" "application/ld+json")  >>>
+      hasAttrValue' "type" "application/ld+json"    >>>
       getChildren                                   >>>
       getText                                       >>.
-      (map C.pack)
+      map C.pack
 
 extractFromHead doc = runX $ doc >>> css "meta" >>>
   (
-    (hasAttrValue' "name"       "pubdate"                       ) <+>
-    (hasAttrValue' "name"       "publishdate"                   ) <+>
-    (hasAttrValue' "name"       "timestamp"                     ) <+>
-    (hasAttrValue' "name"       "dc.date.issued"                ) <+>
-    (hasAttrValue' "property"   "article:published_time"        ) <+>
-    (hasAttrValue' "name"       "date"                          ) <+>
-    (hasAttrValue' "property"   "bt:pubdate"                    ) <+>
-    (hasAttrValue' "name"       "sailthru.date"                 ) <+>
-    (hasAttrValue' "name"       "article.published"             ) <+>
-    (hasAttrValue' "name"       "published-date"                ) <+>
-    (hasAttrValue' "name"       "article.created"               ) <+>
-    (hasAttrValue' "name"       "article_date_original"         ) <+>
-    (hasAttrValue' "name"       "cxenseparse:recs:publishtime"  ) <+>
-    (hasAttrValue' "name"       "date_published"                ) <+>
-    (hasAttrValue' "itemprop"   "datepublished"                 ) <+>
-    (hasAttrValue' "itemprop"   "datecreated"                   ) <+>
-    (hasAttrValue' "name"       "created"                       ) <+>
-    (hasAttrValue' "http-equiv" "date"                          )
+    hasAttrValue' "name"       "pubdate"                        <+>
+    hasAttrValue' "name"       "publishdate"                    <+>
+    hasAttrValue' "name"       "timestamp"                      <+>
+    hasAttrValue' "name"       "dc.date.issued"                 <+>
+    hasAttrValue' "property"   "article:published_time"         <+>
+    hasAttrValue' "name"       "date"                           <+>
+    hasAttrValue' "property"   "bt:pubdate"                     <+>
+    hasAttrValue' "name"       "sailthru.date"                  <+>
+    hasAttrValue' "name"       "article.published"              <+>
+    hasAttrValue' "name"       "published-date"                 <+>
+    hasAttrValue' "name"       "article.created"                <+>
+    hasAttrValue' "name"       "article_date_original"          <+>
+    hasAttrValue' "name"       "cxenseparse:recs:publishtime"   <+>
+    hasAttrValue' "name"       "date_published"                 <+>
+    hasAttrValue' "itemprop"   "datepublished"                  <+>
+    hasAttrValue' "itemprop"   "datecreated"                    <+>
+    hasAttrValue' "name"       "created"                        <+>
+    hasAttrValue' "http-equiv" "date"
   ) >>> getAttrValue "content"
 
 extractFromBody doc = runX $ doc >>>
@@ -69,26 +69,26 @@ extractFromBody doc = runX $ doc >>>
   )
 
 parseTime :: String -> String -> Maybe UTCTime
-parseTime f t = (parseTimeM True) defaultTimeLocale f t :: Maybe UTCTime
+parseTime f t = parseTimeM True defaultTimeLocale f t :: Maybe UTCTime
 
 extractPublishedDates :: [C.ByteString] -> [String]
-extractPublishedDates a = map datePublished $ mapMaybe (decode) a
+extractPublishedDates a = map datePublished $ mapMaybe decode a
 
 parseDates :: [String] -> [UTCTime]
-parseDates ds = mapMaybe (\d ->
-                              (parseTime "%Y-%m-%d"                 d)  <|>
-                              (parseTime "%B %e, %Y"                d)  <|>
-                              (parseTime "%Y-%m-%d %H:%M:%S"        d)  <|>
-                              (parseTime "%Y-%m-%dT%H:%M:%S%Z"      d)  <|>
-                              (parseTime "%B %k, %Y, %H:%M %p"      d)  <|>
-                              (parseTime "%Y-%m-%d %H:%M:%S.000000" d)
-                        ) ds
+parseDates = mapMaybe (\d ->
+                              parseTime "%Y-%m-%d"                 d  <|>
+                              parseTime "%B %e, %Y"                d  <|>
+                              parseTime "%Y-%m-%d %H:%M:%S"        d  <|>
+                              parseTime "%Y-%m-%dT%H:%M:%S%Z"      d  <|>
+                              parseTime "%B %k, %Y, %H:%M %p"      d  <|>
+                              parseTime "%Y-%m-%d %H:%M:%S.000000" d
+                        )
 
 fromHtml :: String -> IO(Maybe UTCTime)
 fromHtml html = do
-  doc       <- return $ parseHtml html
+  let doc = parseHtml html
   scripts   <- extractFromLdJson doc
-  ldJson    <- return $ extractPublishedDates scripts
+  let ldJson = extractPublishedDates scripts
   heads     <- extractFromHead doc
   bodies    <- extractFromBody doc
   return $ listToMaybe $ parseDates $ heads ++ bodies ++ ldJson
@@ -96,5 +96,4 @@ fromHtml html = do
 fromUrl :: String -> IO(Maybe UTCTime)
 fromUrl url = do
   body <- readUrl url
-  date <- fromHtml body
-  return date
+  fromHtml body
